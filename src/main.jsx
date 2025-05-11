0d0415cc625585e83cac0098b6622f6170b83fe2
Cpiggott-lab <crpapc@gmail.com>
2025-05-11 13:02:39 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 
+const base = import.meta.env.MODE === "production" ? "/To-Do-App" : "/";
+
 createRoot(document.getElementById("root")).render(
-  <Router basename="/To-Do-App">
+  <Router basename={base}>
     <StrictMode>
       <App />
     </StrictMode>
